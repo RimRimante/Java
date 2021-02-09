6fc5af7ca0a9e1aa31c3e324050eb5f0ab2df2cb
RimRimante <andrimante@gmail.com>
2021-02-09 18:59:38 +0200

    Introduction to Java

---
+++ intro1/MyProfile.java
@@ -0,0 +1,14 @@
+public class MyProfile {
+	public static void main(String[] args) {   
+    String name = "Rimante";
+    int age = 19;
+    double desiredSalary = 1000000;
+    char gender = 'f';
+    boolean lookingForJob=true;
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(gender);
+    System.out.println(lookingForJob);
+    System.out.println(desiredSalary);
+	}
+}
